a711e98876264440aac9d46cc7d8759c535df57b
relinex <97308126+codekaann@users.noreply.github.com>
2026-04-07 23:02:40 +0300

    Update ip.js

--- src/commands/user/ip.js
+++ src/commands/user/ip.js
@@ -16,7 +16,7 @@ data: {
 
         const embeda = new EmbedBuilder()
             .setColor('Random')
-            .setAuthor({name: `${settings.sunucu.isim}`, iconURL: "https://cdn.discordapp.com/attachments/1119010537141981324/1137474688642207834/M.png" + settings.sunucu.ip})
+            .setAuthor({name: `${settings.sunucu.isim}`, iconURL: "https://cdn.discordapp.com/icons/984874881869029448/63a8540a14cd6638b259bb4dde84fd47.png" + settings.sunucu.ip})
             .setDescription(`▸ ${settings.sunucu.ip}`)
             .setThumbnail("https://eu.mc-api.net/v3/server/favicon/" + settings.sunucu.ip)
             .setFooter({text: settings.sunucu.ip})
@@ -28,7 +28,7 @@ data: {
             
             const embeda = new EmbedBuilder()
                 .setColor('Random')
-                .setAuthor({name: `${settings.sunucu.isim}`, iconURL: "https://cdn.discordapp.com/attachments/1119010537141981324/1137474688642207834/M.png" + settings.sunucu.ip})
+                .setAuthor({name: `${settings.sunucu.isim}`, iconURL: "https://cdn.discordapp.com/icons/984874881869029448/63a8540a14cd6638b259bb4dde84fd47.png" + settings.sunucu.ip})
                 .setDescription(`▸ ${settings.sunucu.ip}`)
                 .setThumbnail("https://eu.mc-api.net/v3/server/favicon/" + settings.sunucu.ip)
                 .setFooter({text: settings.sunucu.ip})
